866396735f3fc3022f9f5deb8bfb3326c3ea338f
rehmanli1470ww <144922823+rehmanli1470ww@users.noreply.github.com>
2023-10-25 22:48:25 +0400

    Add project files.

---
+++ WpfAppAzeriCat/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppAzeriCat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Yazisma> ?yazismaq { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            yazismaq = new List<Yazisma>() {
+                new Yazisma(){
+                    Date="12.30",
+                    Text="Salam necesen"
+                },
+                new Yazisma(){
+                    Date="12.32",
+                    Text="Aleykum Salam"
+                },
+                new Yazisma(){
+                    Date="12.32",
+                    Text="Aleykum Salam"
+                },
+                new Yazisma(){
+                    Date="12.30",
+                    Text="Salam necesen"
+                },
+                new Yazisma(){
+                    Date="12.32",
+                    Text="Aleykum Salam"
+                },
+                new Yazisma(){
+                    Date="12.32",
+                    Text="Aleykum Salam"
+                },
+                new Yazisma(){
+                    Date="12.30",
+                    Text="Salam necesen"
+                },
+                new Yazisma(){
+                    Date="12.32",
+                    Text="Aleykum Salam"
+                },
+                new Yazisma(){
+                    Date="12.32",
+                    Text="Aleykum Salam"
+                },
+                new Yazisma(){
+                    Date="12.30",
+                    Text="Salam necesen"
+                },
+                new Yazisma(){
+                    Date="12.32",
+                    Text="Aleykum Salam"
+                },
+                new Yazisma(){
+                    Date="12.32",
+                    Text="Aleykum Salam"
+                },
+                new Yazisma(){
+                    Date="12.30",
+                    Text="Salam necesen"
+                },
+                new Yazisma(){
+                    Date="12.32",
+                    Text="Aleykum Salam"
+                },
+                new Yazisma(){
+                    Date="12.32",
+                    Text="Aleykum Salam"
+                },
+                new Yazisma(){
+                    Date="12.30",
+                    Text="Salam necesen"
+                },
+                new Yazisma(){
+                    Date="12.32",
+                    Text="Aleykum Salam"
+                },
+                new Yazisma(){
+                    Date="12.32",
+                    Text="Aleykum Salam"
+                }
+            };
+            DataContext = this;
+        }
+    }
+}
---
+++ WpfAppAzeriCat/Yazisma.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppAzeriCat
+{
+    public class Yazisma
+    {
+        public string? Date { get; set; }
+        public string? Text { get; set; }
+
+        public override string ToString()
+        {
+            return $@"{Date} {Text}";
+        }
+    }
+    
+}
